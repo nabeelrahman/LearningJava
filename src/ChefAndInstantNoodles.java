6c2c4b386e8c2f0f303eebc9458593dbe35b53e9
Nabeel <nabeelrahman@hotmail.com>
2024-01-31 10:44:58 -0800

    Adding HashMap

---
+++ src/ChefAndInstantNoodles.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class ChefAndInstantNoodles {
+
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner scn = new Scanner(System.in);
+            int stoves = scn.nextInt();
+            int packets = scn.nextInt();
+
+            System.out.println(stoves*packets);
+            // your code goes here
+
+        }
+    }
+
