e3abdc0089bd8807a7c79f1db16b25f9e3b2c6a3
Nabeel <nabeelrahman@hotmail.com>
2024-02-05 21:51:58 -0800

    adding tests

--- src/HashMapCreateAndInsert.java
+++ src/HashMapCreateAndInsert.java
@@ -5,6 +5,18 @@ public class HashMapCreateAndInsert {
 
     public static void main (String args[]) {
 
-        Map<String, Integer>
+        Map<String, Integer> stockPrice = new HashMap<>();
+
+        stockPrice.put("Oracle", 15);
+        System.out.println(stockPrice);
+        stockPrice.put("Oracle", 18);
+        System.out.println(stockPrice);
+        stockPrice.put("Oracle", 20);
+        System.out.println(stockPrice);
+
+
+
+
+
     }
 }
