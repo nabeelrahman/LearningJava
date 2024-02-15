91b10ead45bfa705f329c2650930eaaef3dd4a6d
Nabeel <nabeelrahman@hotmail.com>
2024-02-14 22:56:17 -0800

    adding Hash Map

--- src/HashMapDemo.java
+++ src/HashMapDemo.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class HashMapDemo {
+import java.util.*;
+import java.util.Map.Entry;
+
+public class HashMapDemo {
+
+    public static void main(String args[]) {
+
+        Map<String, Integer> stockPrice = new HashMap<>();
+
+        stockPrice.put("Oracle", 56);
+        stockPrice.put("Fiserv", 117);
+        stockPrice.put("BMW", 73);
+        stockPrice.put("Microsoft", 213);
+        stockPrice.put("Google", 421);
+        stockPrice.put("Ford", 456);
+        stockPrice.put("Novartis", 43);
+        stockPrice.put("TCS", 23);
+
+        System.out.println(stockPrice);
+
+        int max = 0;
+        String company = "";
+
+        for (Entry<String, Integer> entry : stockPrice.entrySet()) {
+            if (entry.getValue() > max) {
+                company = entry.getKey();
+                max = entry.getValue();
+            }
+        }
+
+        //Highest Stock Price
+        System.out.println("Company stock price is " + company);
+        int sum = 0;
+
+        //Average Stock Price
+        for (Entry<String, Integer> entry : stockPrice.entrySet()) {
+            sum = sum + entry.getValue();
+        }
+        System.out.println("Average sum is: " + sum / stockPrice.size());
+
+        //Remove companies stock price below 50.00
+        for (Entry<String, Integer> entry : stockPrice.entrySet()) {
+            if (entry.getValue() < 50) {
+                stockPrice.remove(entry.getKey(), entry.getValue());
+            }
+        }
+    }
 }
--- src/HashMapUpdateAndRemoval.java
+++ src/HashMapUpdateAndRemoval.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class HashMapUpdateAndRemoval {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapUpdateAndRemoval {
+
+    public static void main (String args[]) {
+
+        Map<String, Integer> stockPrice = new HashMap<>();
+        stockPrice.put("", 34);
+        stockPrice.put("Oracle", 434);
+        stockPrice.put("NVDIA", 334);
+        stockPrice.put("FB", 234);
+        stockPrice.put("APPLE", 134);
+        System.out.println(stockPrice);
+
+        //Get oracle
+        System.out.println(stockPrice.get("Oracle"));
+        //Get FB
+        System.out.println(stockPrice.get("FB"));
+        //Get value if not present and assign a default value
+        System.out.println(stockPrice.getOrDefault("GOOGLE", 100));
+        System.out.println(stockPrice);
+        //Replace a value using replace(K key, V value):
+        System.out.println(stockPrice.replace("APPLE", 1034));
+        System.out.println(stockPrice.get("APPLE"));
+        stockPrice.replaceAll((k,v) -> v+10);
+        System.out.println(stockPrice);
+
+        //Cheking if key or value exists
+        System.out.println(stockPrice.containsKey("SHIFT"));
+        System.out.println(stockPrice.containsValue(1044));
+
+        Set<String> keys  = stockPrice.keySet();
+        for(String key : keys ){
+            System.out.println(keys);
+        }
+
+    }
 }
